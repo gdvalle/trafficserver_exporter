46273275e1803cd0913eefc03b220ff9ed725935
Greg Dallavalle <greg.dallavalle@gmail.com>
2018-06-25 20:23:52 -0500

    Use pytest, fix tests

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import os
+import json
+import glob
+
+import pytest
+
+
+DATA_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+)
+
+STATS_JSON_FILES = []
+for fname in glob.glob("{}{}{}".format(DATA_DIR, os.sep, "*.json")):
+    with open(fname) as f:
+        STATS_JSON_FILES.append(json.loads(f.read()))
+
+
+@pytest.fixture(scope="session", params=STATS_JSON_FILES)
+def stats_json(request):
+    return request.param
--- tests/test_stats_plugin_collector.py
+++ tests/test_stats_plugin_collector.py
@@ -1,21 +1,9 @@
-import unittest
-import os
-import json
+import prometheus_client
 
 from trafficserver_exporter.collector import StatsPluginCollector
 
 
-SCRIPT_DIR, _ = os.path.split(__file__)
-DATA_DIR = os.path.join(SCRIPT_DIR, 'data')
-
-STATS_JSON = os.path.join(DATA_DIR, 'stats_over_http.json')
-
-
-class StatsPluginCollectorTestCase(unittest.TestCase):
-    def setUp(self):
-        with open(STATS_JSON) as f:
-            self.stats_json = json.loads(f.read())
-
-    def test_metric_parser(self):
-        collector = StatsPluginCollector('')
-        collector.parse_metrics(self.stats_json['global'])
+def test_metric_parser(stats_json):
+    collector = StatsPluginCollector("")
+    for metric in collector.parse_metrics(stats_json["global"]):
+        assert isinstance(metric, prometheus_client.core.Metric)
